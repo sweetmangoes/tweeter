6253d7520098e4cc0ed881a890f0e22f68a70957
sweetmangoes <alphonsechristian@gmail.com>
2022-11-23 10:17:01 -0500

    created script for tweet-container hover functionality, jquery is initialized

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  console.log(`Jquery works!`);
+  console.log(`Jquery typing active!`);
   $("#tweet-text").keyup(function (event) {
     console.log(`typing works!`);
     let charCount = $(this).val().length;
