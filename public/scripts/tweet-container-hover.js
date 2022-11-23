3fc5c91a44304c11863666ad2fbfb1f8a8eb7404
sweetmangoes <alphonsechristian@gmail.com>
2022-11-23 12:55:32 -0500

    script tweet-hover-container.jss generates a box shadow when hovering in the tweet container, gold color when icon are being hovered upon

--- public/scripts/tweet-container-hover.js
+++ public/scripts/tweet-container-hover.js
@@ -1,3 +1,35 @@
 $(document).ready(function () {
-  console.log(`Jquery hover active!`)
-})
+  console.log(`Jquery hover active!`);
+  $("#tweet-container").hover(
+    function () {
+      $(this).css("box-shadow", "15px 15px  #4057a16f");
+    },
+    function () {
+      $(this).css("box-shadow", "");
+    }
+  );
+  $("#icon-flag").hover(
+    function () {
+      $(this).css("color", "gold");
+    },
+    function () {
+      $(this).css("color", "");
+    }
+  );
+  $("#icon-retweet").hover(
+    function () {
+      $(this).css("color", "gold");
+    },
+    function () {
+      $(this).css("color", "");
+    }
+  );
+  $("#icon-like").hover(
+    function () {
+      $(this).css("color", "gold");
+    },
+    function () {
+      $(this).css("color", "");
+    }
+  );
+});
