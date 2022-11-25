da736024f6cc4a389b187f62812dfac57eefa1a7
sweetmangoes <alphonsechristian@gmail.com>
2022-11-24 20:09:00 -0500

    post route script works for submitting new tweets

--- server/index.js
+++ server/index.js
@@ -35,3 +35,7 @@ app.use("/tweets", tweetsRoutes);
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
+app.post("/", function(req, res){
+  res.send(`it is working`)
+})
